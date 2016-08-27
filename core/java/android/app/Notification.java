6e23da0373c7673550d8efe7de7de22969a2e46b
Nicholas Chum <nicholaschum@gmail.com>
2016-08-27 10:56:46 -0400

    Themes: Notifications: Expose a bool to disable dynamic colors
    forward port to oreo
    
    This commit allows a themer to overlay a boolean value in config.xml to
    disable dynamic colors applied to the app title and app icon of each
    notification.
    
    PS6:
    Separate app title and small icon colors for more flexibility.
    Expose hardcoded sender text name.
    
    Oreo edits:
    follow bool for OverflowNumbers as well
    before: https://i.imgur.com/JqeLFD1.jpg
    after: https://i.imgur.com/GPoM0zo.jpg
    
    Change-Id: I3c7828118991ec4fc616011caf073c81f75428b4

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -4840,8 +4840,8 @@ public class Notification implements Parcelable
                 boolean ambient) {
             boolean colorable = !isLegacy() || getColorUtil().isGrayscaleIcon(mContext, smallIcon);
             int color;
-            if (!mAllowIconTextTint) {
-                color = ambient ? resolveAmbientColor() : mThemeContext.getColor(R.color.notification_icon_default_color);
+            if (!Resources.getSystem().getBoolean(R.bool.config_allowNotificationIconTextTinting)) {
+                color = ambient ? resolveAmbientColor() : mContext.getColor(R.color.notification_icon_default_color);
             } else {
                 color = ambient ? resolveAmbientColor() : getPrimaryHighlightColor();
             }
@@ -4875,20 +4875,20 @@ public class Notification implements Parcelable
         }
 
         int getSenderTextColor() {
-            return mThemeContext.getColor(R.color.sender_text_color);
+            return mContext.getColor(R.color.sender_text_color);
         }
 
         int resolveIconContrastColor() {
-            if (!mAllowIconTextTint) {
-                return mThemeContext.getColor(R.color.notification_icon_default_color);
+            if (!Resources.getSystem().getBoolean(R.bool.config_allowNotificationIconTextTinting)) {
+                return mContext.getColor(R.color.notification_icon_default_color);
             } else {
                 return resolveContrastColor();
             }
         }
 
         int resolveContrastColor() {
-            if (!mAllowIconTextTint) {
-                return mThemeContext.getColor(R.color.notification_text_default_color);
+            if (!Resources.getSystem().getBoolean(R.bool.config_allowNotificationIconTextTinting)) {
+                return mContext.getColor(R.color.notification_text_default_color);
             }
 
             if (mCachedContrastColorIsFor == mN.color && mCachedContrastColor != COLOR_INVALID) {
@@ -6270,7 +6270,7 @@ public class Notification implements Parcelable
                 sb.append(bidi.unicodeWrap(m.mSender),
                         makeFontColorSpan(colorize
                                 ? builder.getPrimaryTextColor()
-                                : mBuilder.resolveContrastColor()),
+                                : builder.getSenderTextColor()),
                         0 /* flags */);
             }
             CharSequence text = m.mText == null ? "" : m.mText;
