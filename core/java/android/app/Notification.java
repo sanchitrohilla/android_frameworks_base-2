7ea3e6011ee076a02add0f52cc02f567782dab26
sanchitrohilla <sanchitrohilla64@gmail.com>
2018-04-19 14:10:25 +0000

    Revert "Themes: Notifications: Expose a bool to disable dynamic colors"
    This reverts commit 59ac6f7dbab70ac2b08949ae9c3ea25c50787553.

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -4840,8 +4840,8 @@ public class Notification implements Parcelable
                 boolean ambient) {
             boolean colorable = !isLegacy() || getColorUtil().isGrayscaleIcon(mContext, smallIcon);
             int color;
-            if (!Resources.getSystem().getBoolean(R.bool.config_allowNotificationIconTextTinting)) {
-                color = ambient ? resolveAmbientColor() : mContext.getColor(R.color.notification_icon_default_color);
+            if (!mAllowIconTextTint) {
+                color = ambient ? resolveAmbientColor() : mThemeContext.getColor(R.color.notification_icon_default_color);
             } else {
                 color = ambient ? resolveAmbientColor() : getPrimaryHighlightColor();
             }
@@ -4875,20 +4875,20 @@ public class Notification implements Parcelable
         }
 
         int getSenderTextColor() {
-            return mContext.getColor(R.color.sender_text_color);
+            return mThemeContext.getColor(R.color.sender_text_color);
         }
 
         int resolveIconContrastColor() {
-            if (!Resources.getSystem().getBoolean(R.bool.config_allowNotificationIconTextTinting)) {
-                return mContext.getColor(R.color.notification_icon_default_color);
+            if (!mAllowIconTextTint) {
+                return mThemeContext.getColor(R.color.notification_icon_default_color);
             } else {
                 return resolveContrastColor();
             }
         }
 
         int resolveContrastColor() {
-            if (!Resources.getSystem().getBoolean(R.bool.config_allowNotificationIconTextTinting)) {
-                return mContext.getColor(R.color.notification_text_default_color);
+            if (!mAllowIconTextTint) {
+                return mThemeContext.getColor(R.color.notification_text_default_color);
             }
 
             if (mCachedContrastColorIsFor == mN.color && mCachedContrastColor != COLOR_INVALID) {
@@ -6270,7 +6270,7 @@ public class Notification implements Parcelable
                 sb.append(bidi.unicodeWrap(m.mSender),
                         makeFontColorSpan(colorize
                                 ? builder.getPrimaryTextColor()
-                                : builder.getSenderTextColor()),
+                                : mBuilder.resolveContrastColor()),
                         0 /* flags */);
             }
             CharSequence text = m.mText == null ? "" : m.mText;
