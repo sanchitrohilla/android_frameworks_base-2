ABC-KANG/abc_frameworks_base

Ticker observer code cleanup
Change-Id: I81d8b51ba79013f8b0bf7858299e2143b1656ef3

packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -88,10 +88,6 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         mKeyguardMonitor = Dependency.get(KeyguardMonitor.class);
         mNetworkController = Dependency.get(NetworkController.class);
         mStatusBarComponent = SysUiServiceProvider.getComponent(getContext(), StatusBar.class);
-
-        mTickerEnabled = Settings.System.getIntForUser(mContentResolver,
-                Settings.System.STATUS_BAR_SHOW_TICKER, 1,
-                UserHandle.USER_CURRENT);
         mTickerObserver = new TickerObserver(new Handler());
     }
 
@@ -145,7 +141,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         initEmergencyCryptkeeperText();
 
         mTickerObserver.observe();
-        initTickerView();
+        mTickerObserver.update();
 
     }
 

sanchitrohilla/android_frameworks_base-2

packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -95,10 +95,6 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         mNetworkController = Dependency.get(NetworkController.class);
         mStatusBarComponent = SysUiServiceProvider.getComponent(getContext(), StatusBar.class);
         mSettingsObserver = new SettingsObserver(new Handler());
-
-        mTickerEnabled = Settings.System.getIntForUser(mContentResolver,
-                Settings.System.STATUS_BAR_SHOW_TICKER, 1,
-                UserHandle.USER_CURRENT);
     }
 
     class SettingsObserver extends UserContentObserver {
@@ -195,7 +191,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         mNetworkTraffic = mStatusBar.findViewById(R.id.networkTraffic);
 
         mSettingsObserver.observe();
-        initTickerView();
+        mSettingsObserver.update();
 
     }
 